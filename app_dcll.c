b15200dfa4db5bdbae90ed4d2215da494f2280e5
NavroopKaur <watwanichitra@gmail.com>
2015-01-13 12:48:01 +0530

    GPLv3 license notice added

--- app_dcll.c
+++ app_dcll.c
@@ -1,3 +1,15 @@
+/*Notification-Junction is an Interface between multiple Applications and multiple Notification Providers.
+Copyright (C) 2015  Navroop Kaur<watwanichitra@gmail.com> , Shivani Marathe<maratheshivani94@gmail.com> , Kaveri Sangale<sangale.kaveri9@gmail.com>
+	All Rights Reserved
+	
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+   
+ You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA 
+*/
+
+
 /* 
 *	The file contains the code for the dcll implementation with trailing singly linked lists for
 *	Initializing app_list, 
